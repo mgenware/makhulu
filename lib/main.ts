6dbe5a4b42a0d280b7c78743d1e83e3deab7945c
Mgen <m@mgenware.com>
2017-09-30 19:08:25 +0800

    Export State class

--- lib/main.ts
+++ lib/main.ts
@@ -2,4 +2,5 @@ export { default as IReporter } from './reporter';
 export { default as DefaultReporter } from './reporters/defaultReporter';
 export { default as Context } from './context';
 export { default as js } from './factory/js';
+export { default as State } from './state';
 export { default as Task } from './task';
