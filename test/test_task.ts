6da2ec805f4ff6173446cdfbc1802f7c3d6c69e8
Mgen <m@mgenware.com>
2017-10-04 18:57:43 +0800

    Test cases for then

--- test/test_task.ts
+++ test/test_task.ts
@@ -1,9 +1,9 @@
 import * as ma from '../lib/main';
 import * as assert from 'assert';
-const ARRAY = [1, 4, 7, 2, -5];
+const VALUES = [1, 4, 7, 2, -5];
 
 function createTask() {
-  const task = ma.js.array(ARRAY);
+  const task = ma.js.array(VALUES);
   return task;
 }
 function arrayToMap(array: number[]): { [key: number]: boolean } {
@@ -12,6 +12,13 @@ function arrayToMap(array: number[]): { [key: number]: boolean } {
     map[n] = true;
   }
   return map;
+}
+
+function assertStates(values: any, states: ma.State[], expectedValues: any[]) {
+  assert.deepEqual(values, expectedValues);
+  for (let i = 0; i < expectedValues.length; i++) {
+    assert.equal(expectedValues[i], states[i].data);
+  }
 }
 
 describe('Task.arguments', () => {
@@ -24,38 +31,38 @@ describe('Task.arguments', () => {
       indices[index] = true;
       assert.equal(state.data, value);
     }).then(() => {
-      assert.deepEqual(values, arrayToMap(ARRAY));
+      assert.deepEqual(values, arrayToMap(VALUES));
       done();
     });
   });
 });
 
 describe('Task.then', () => {
-  it('prevValues', (done) => {
+  it('then should reflect task\'s values and states', (done) => {
     const task = createTask();
     task.then((values, states) => {
-      assert.deepEqual(values, task.states.map(state => state.data));
-      assert.deepEqual(states, task.states);
+      assertStates(values, states, VALUES);
     }).then(() => {
       done();
     });
   });
-  it('return undefined', (done) => {
+  it('Return undefined', (done) => {
     const task = createTask();
-    let prevValues: any[] = null;
+    let currentValues: number[] = [];
     task.then((values, states) => {
-      prevValues = values;
+      currentValues = values;
     }).then((values) => {
-      assert.deepEqual(prevValues, values);
+      assert.deepEqual(values, currentValues);
       done();
     });
   });
-  it('return states', (done) => {
+  it('Return another set of states', (done) => {
     const task = createTask();
+    const sampleValue = [4, 5, 6];
     task.then((values, states) => {
-      prevValues = values;
+      return states.filter(s => s.data < 0);
     }).then((values) => {
-      assert.deepEqual(prevValues, values);
+      assert.deepEqual(values, [-5]);
       done();
     });
   });
