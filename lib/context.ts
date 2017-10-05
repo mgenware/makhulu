3cc44959db05ba95849f5ae571e323268b6db512
Mgen <m@mgenware.com>
2017-10-05 21:09:52 +0800

    Use a dictionary-based context

--- lib/context.ts
+++ lib/context.ts
@@ -1,3 +1,11 @@
 export default class Context {
-  constructor(public name: string, public value: any) { }
+  map: { [key: string] : any; } = {};
+  
+  getValue(key: string): any {
+    return this.map[key];
+  }
+
+  setValue(key: string, value: any) {
+    this.map[key] = value;
+  }
 }
