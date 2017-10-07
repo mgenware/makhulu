efde3eb593c9fc51e2f282b6a9cf35c3114f5ef4
Mgen <m@mgenware.com>
2017-10-07 19:48:01 +0800

    Add module importing test

---
+++ test/test_module.ts
@@ -0,0 +1,11 @@
+const main = require('../..');
+import * as assert from 'assert';
+
+describe('require this module', () => {
+  it('No exception is thrown', () => {
+    assert.doesNotThrow(() => {
+      const task = main.js.array([]);
+      assert.notEqual(task.reporter, null);
+    });
+  });
+});  
