50f97efbd18fa68a898e9ce05b8b0725632d9e73
Mgen <m@mgenware.com>
2017-10-08 17:04:05 +0800

    Add tests for readFileAsync and writeFileAsync

--- test/test_factory_fs.ts
+++ test/test_factory_fs.ts
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
 import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
 import * as ma from '../lib/main';
 
 function arrayToMap(array: string[]): { [key: number]: boolean } {
@@ -23,8 +25,32 @@ function testGlob(title: string, glob: string, files: string[]) {
   });
 }
 
+function appendFile(s: string): string {
+  return path.join(__dirname, s);
+}
+
 describe('Factory.FS.fileNames', () => {
   testGlob('All files', '**/*.*', ['test.json', 'dir/test.json', 'test.txt']);
   testGlob('Direct children', '*.*', ['test.json', 'test.txt']);
   testGlob('Custom filter', '**/*.json', ['test.json', 'dir/test.json']);
+});
+
+describe('Factory.FS.readFileAsync', () => {
+  it('Read a file', (done) => {
+    ma.fs.readFileAsync(appendFile('data/test.txt'), 'utf8').then((value: Buffer) => {
+      assert.equal(value.toString(), 'sample text\n');
+      done();
+    });
+  });
+});
+
+describe('Factory.FS.writeFileAsync', () => {
+  it('Write a file', (done) => {
+    const filePath = path.join(os.tmpdir(), 'makhulu/dir1/dir2/test.txt');
+    const content = 'hello makhulu';
+    ma.fs.writeFileAsync(filePath, content).then(() => {
+      assert.equal(fs.readFileSync(filePath, 'utf8'), content);
+      done();
+    });
+  });
 });
