e6c1688ef154540605306cd18cdf3e2eeb8f5210
Mgen <m@mgenware.com>
2018-11-10 20:53:31 +0800

    Update module test to async

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,5 +1,7 @@
+import { promisify } from 'util';
 import * as fs from 'fs';
+const statAsync = promisify(fs.stat);
 
-test('Verify type definition files', () => {
-  expect(fs.statSync('./dist/main.d.ts').isFile()).toBeTruthy();
+test('Verify type definition files', async () => {
+  expect((await statAsync('./dist/main.d.ts')).isFile()).toBeTruthy();
 });
