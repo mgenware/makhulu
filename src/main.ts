dc7620019e90f749c56d40d326b96512c2a263e6
Mgen <m@mgenware.com>
2018-11-10 21:18:50 +0800

    Export fileSystem as fs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 export { default as DataItem } from './dataItem';
 export { default as DataList } from './dataList';
-export { default as fileSystem } from './fs';
+export { default as fs } from './fs';
