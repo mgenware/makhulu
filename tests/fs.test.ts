70de86e0942ee38afbc08cc168169451e0023fee
Mgen <m@mgenware.com>
2018-11-10 21:17:47 +0800

    Verify typeInfo not changing after mapAsync

--- tests/fs.test.ts
+++ tests/fs.test.ts
@@ -20,8 +20,16 @@ test('glob', async () => {
   expect(fileData.values().sort()).toEqual(['a.txt', 'c.txt'].map(f => GlobFiles + f).sort());
 });
 
-test('fileToContentString', async () => {
-  const fileData = await fileSystem.globAsync(`${GlobFiles}*.txt`);
+test('All files, fileToContentString, typeInfo', async () => {
+  const fileData = await fileSystem.globAsync(`${GlobFiles}**/*.txt`);
   await fileData.mapAsync(fileSystem.fileToContentStringAsync);
-  expect(fileData.values()).toEqual(['A\n', 'C\n']);
+  expect(fileData.values()).toEqual(['A\n', 'C\n', 'D\n']);
+
+  // verify type infos not changing
+  const list = fileData.list;
+  expect(list.length).toBe(3);
+  list.sort((a, b) => (a.typeInfo.get('FILE') as string).localeCompare((b.typeInfo.get('FILE') as string)));
+  expect(list[0].typeInfo).toEqual(map({FILE: './tests/glob-files/a.txt'}));
+  expect(list[1].typeInfo).toEqual(map({FILE: './tests/glob-files/c.txt'}));
+  expect(list[2].typeInfo).toEqual(map({FILE: './tests/glob-files/sub/d.txt'}));
 });
