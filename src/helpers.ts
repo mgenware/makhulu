3c4aa1b462068303fccf80a30ed57f053bbcac41
Mgen <m@mgenware.com>
2019-01-07 19:41:37 +0800

    Add mk.count

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,14 @@
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function count(length: number, start = 0): number[] {
+  if (length < 0) {
+    throw new Error('The "length" argument cannot be negative');
+  }
+  const arr = new Array(length);
+  for (let i = 0; i < length; i++) {
+    arr[i] = start + i;
+  }
+  return arr;
+}
--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -3,3 +3,8 @@ import * as mk from '../';
 test('sleep', async () => {
   expect(typeof mk.sleep).toBe('function');
 });
+
+test('count', async () => {
+  expect(await mk.count(4)).toEqual([0, 1, 2, 3]);
+  expect(await mk.count(4, -3)).toEqual([-3, -2, -1, 0]);
+});
